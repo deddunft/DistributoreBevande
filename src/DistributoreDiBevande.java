50d3301bbb9b2e3d2a2b0cc2519fb866fcfa5c6b
deddunft <luca36italia@gmail.com>
2022-03-10 14:57:32 +0100

    à

--- src/DistributoreDiBevande.java
+++ src/DistributoreDiBevande.java
@@ -4,23 +4,29 @@ public class DistributoreDiBevande {
 
     private Bevande[] distrC;
     private Bevande[] distrCp;
+    private int num;
 
     public DistributoreDiBevande(int num){
-        distrC = new Bevande[num];
+        /*distrC = new Bevande[num];
         distrCp = new Bevande[num];
         for (int i = 0; i < num; i++) {
             distrC[i] = new Caffe();
             distrCp[i] = new Cappuccino();
 
+        }*/
+
+        this.num= num;
+    }
+    public void caricaProdotto(Bevande bevande){
+        distrC = new Bevande[num];
+        for (int i = 0; i < num; i++) {
+            distrC[i]= bevande;
         }
+
     }
 
-    @Override
-    public String toString() {
-        return "DistributoreDiBevande{" +
 
-                ", distrC=" + Arrays.toString(distrC) +
-                ", distrCp=" + Arrays.toString(distrCp) +
-                '}';
-    }
+
+
+
 }
