4c4ae840604543aaa4f2a47f3927e61945e38fd0
deddunft <luca36italia@gmail.com>
2022-03-10 15:09:14 +0100

    si

--- src/DistributoreDiBevande.java
+++ src/DistributoreDiBevande.java
@@ -5,6 +5,7 @@ public class DistributoreDiBevande {
     private Bevande[] distrC;
     private Bevande[] distrCp;
     private int num;
+    private double saldo;
 
     public DistributoreDiBevande(int num){
         /*distrC = new Bevande[num];
@@ -25,8 +26,35 @@ public class DistributoreDiBevande {
 
     }
 
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void inserisciImporto(double soldi){
+        saldo = getSaldo()+soldi;
+    }
+
+    public void scegliProdotto(int barc){
+        for (int i = 0; i <num ; i++) {
 
 
+            if (distrC[i].getBarcode()==barc){
+                saldo=getSaldo()-distrC[i].getPrezzo();
+            }
+            if (distrC[i].getBarcode()==barc && distrC[i].getPrezzo()>getSaldo()){
+                System.out.println("Oh poveracciooo!");
+            }
+        }
+    }
 
 
+    @Override
+    public String toString() {
+        return "DistributoreDiBevande{" +
+                "distrC=" + Arrays.toString(distrC) +
+                ", distrCp=" + Arrays.toString(distrCp) +
+                ", num=" + num +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
--- src/MIAN.java
+++ src/MIAN.java
@@ -4,6 +4,16 @@ public class MIAN {
 
     public static void main(String[] args) {
         DistributoreDiBevande db1 = new DistributoreDiBevande(5);
+        Caffe coffi = new Caffe();
         System.out.println(db1);
+        db1.caricaProdotto(coffi);
+
+        db1.inserisciImporto(200);
+        db1.scegliProdotto(24565);
+
+
+        System.out.println(db1);
+
+
     }
 }
